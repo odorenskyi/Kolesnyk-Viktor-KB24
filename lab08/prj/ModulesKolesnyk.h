b61a3e9e869141b437a0353eb0a4da2b665abcd1
Lenavori <kolesnykviktor2006@gmail.com>
2025-03-25 13:56:41 +0200

    Added h header, edited TestSuits and static library

---
+++ lab08/prj/ModulesKolesnyk.h
@@ -0,0 +1,6 @@
+#ifndef MODULESKOLESNYK_H_INCLUDED
+#define MODULESKOLESNYK_H_INCLUDED
+
+double s_calculation(double x, double y, double z);
+
+#endif // MODULESKOLESNYK_H_INCLUDED
--- lab08/prj/ModulesKolesnyk/main.cpp
+++ lab08/prj/ModulesKolesnyk/main.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
 #include <cmath>
 
+using namespace std;
+
 double s_calculation(double x, double y, double z) {
+    if (z < 0) {
+        cout << "Помилка: значення z не може бути від'ємним." << endl;
+        return NAN;
+    }
     double num = pow(x, 2) - pow(y, 3);
     double den = 12 * x + pow(y, 2) - M_PI * sqrt(z);
 
-    if (den == 0 || z < 0) {
+    if (den == 0) {
+        cout << "Помилка: знаменник не може бути нульовим." << endl;
         return NAN;
     }
     return num + sqrt(abs(z * exp(x)) / den);
